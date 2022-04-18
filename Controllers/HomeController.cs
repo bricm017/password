ac4e63944b82cf8efd827b9bfdb14d30c2d20337
bricm017 <madisonlbrice@gmail.com>
2022-04-17 21:11:50 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using password.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace password.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            int? count = HttpContext.Session.GetInt32("count");
+            if(count == null)
+            {
+                count = 0;
+            }
+            count += 1;
+            string result = "";
+            string letter_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            string passcode = "";
+            Random rand = new Random();
+            for(var i = 0; i < 14; i++)
+            {
+                passcode = passcode +letter_list[rand.Next(0,letter_list.Length)];
+            }
+            ViewBag.passcode = passcode;
+            ViewBag.CountNumber = count;
+            HttpContext.Session.SetInt32("count", (int)count);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
